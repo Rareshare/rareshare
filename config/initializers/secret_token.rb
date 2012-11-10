6e0510979e283e62d967ae477c621ec4275dfa42
Brian Guthrie <btguthrie@gmail.com>
2012-11-10 14:58:37 -0500

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rareshare::Application.config.secret_token = '7ed84812e1809ced4412c577a535f30bd6ef6a904d7e9ae523e1405faf082b44d28d52034cc67c986076be91db0c9211ce9adbf7ac14830626e2bcaa2244d65a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rareshare::Application.config.session_store :cookie_store, key: '_rareshare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rareshare::Application.config.session_store :active_record_store
