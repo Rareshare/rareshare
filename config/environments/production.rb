24fc6f1387b470718a4b0281bbaa11766c8c5e55
Brian Guthrie <btguthrie@gmail.com>
2012-11-25 13:04:28 -0500

    Setting default URL in production to the Heroku site for the time being.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,8 @@ Rareshare::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  config.action_mailer.default_url_options = { :host => 'rareshare.herokuapp.com' }
+  config.action_controller.default_url_options = { :host => 'rareshare.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
