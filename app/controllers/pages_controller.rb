c390728997465e67a3495005c9bdcf74cbb581b9
Brian Guthrie <btguthrie@gmail.com>
2014-03-18 15:45:16 -0400

    Since find_by doesn't seem to work with slugs anymore, manually update the finder.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
 
   def show
-    @page = Page.find(params[:page])
+    @page = Page.find(params[:page]) || Page.where(slug: params[:page]).first
     not_found if @page.nil?
   end
 
