6d2a596bc42232fbaa7b5e520cda31b30442fb2c
Sang Won Seo <sangseo89@gmail.com>
2014-04-07 19:08:46 -0400

    fixes admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,9 +3,6 @@ class AdminMailer < ActionMailer::Base
 
   def new_user_email(user_id)
     @user = User.find(user_id)
-
-    User.admin.each do |admin|
-      mail to: admin.email, subject: "New user registered"
-    end
+    mail to: User.admin.map(&:email).join(', '), subject: "New user registered"
   end
 end
