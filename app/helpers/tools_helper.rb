364138edc358fc1aae1fec4eb36dbd483db733ad
Michael Pavling <pavling@gmail.com>
2014-12-30 09:44:22 +0100

    Altered the 'my tools' view to say 'per sample' rather than showing the lead-time and expedite-time

--- app/helpers/tools_helper.rb
+++ app/helpers/tools_helper.rb
@@ -15,9 +15,9 @@ module ToolsHelper
   end
 
   def format_per_sample_tool_price(tp, t)
-    base = "%s: #{t.currency_symbol}%0.02f/%d days" % [ tp.label, tp.base_price, tp.lead_time_days || 0 ]
+    base = "%s: #{t.currency_symbol}%0.02f per sample" % [ tp.label, tp.base_price ]
     if tp.can_expedite?
-      base << ", #{t.currency_symbol}%0.02f/%d days" % [ tp.expedite_price, tp.expedite_time_days || 0 ]
+      base << ", #{t.currency_symbol}%0.02f per sample" % [ tp.expedite_price]
     end
     base
   end
