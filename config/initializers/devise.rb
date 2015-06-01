ae8c764ee18bf34b986e761bf4d72719725fd486
Pavling <git@pavling.co.uk>
2015-06-01 10:13:21 +0100

    altering the linked-in scopes due to their api changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -206,7 +206,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :linkedin, ENV['LINKEDIN_API_KEY'], ENV['LINKEDIN_SECRET_KEY'], scope: "r_emailaddress,r_fullprofile,r_contactinfo"
+  config.omniauth :linkedin, ENV['LINKEDIN_API_KEY'], ENV['LINKEDIN_SECRET_KEY'], scope: "r_emailaddress"
+  # Removed "r_fullprofile" and "r_contactinfo" scopes due to linked-in's api changes
+  # https://developer.linkedin.com/support/developer-program-transition
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
